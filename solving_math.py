06761595f5d1938665a8fd03a71470615beae48d
Ubuntu <ubuntu@ip-172-31-7-96.ec2.internal>
2021-10-21 16:22:39 +0000

    some commit message

---
+++ solving_math.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python3
+
+# Created by Joshua Yeung
+# Created on September 2021
+# This program solving basic math question
+
+
+def main():
+    # this function solving basic math question
+
+    # input
+    first_number = int(input("Enter first number: "))
+    second_number = int(input("Enter second number: "))
+
+    # process
+    answer = first_number + second_number
+
+    # output
+    print("")
+    print("The answer is {}.".format(answer))
+
+
+if __name__ == "__main__":
+    main()
